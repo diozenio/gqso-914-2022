01191179e207437cdfb49e3c591cb4de9ea7114c
diozenio <d.dionisiobsl@gmail.com>
2022-06-27 20:49:42 -0300

    Context de Soma pronto

--- Soma.java
+++ Soma.java
@@ -1,28 +1,50 @@
 import java.io.IOException;
-
+import java.util.List;
+import java.util.ArrayList;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-public class Soma implements HttpHandler{
+public class Soma implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         try {
             String[] partes = exchange.getRequestURI().getPath().split("/");
-            if (partes[partes.length-1].equals("java")) {
-                byte[] resposta = "Tu que eh".getBytes();
-                exchange.sendResponseHeaders(200, resposta.length);
-                exchange.getResponseBody().write(resposta);
-                return;
-            } 
-            
-            byte[] resposta = partes[partes.length-1].getBytes();
+            List<Double> numeros = new ArrayList<Double>();
+            for (int i = 2; i < partes.length; i++) {
+                boolean isNumeric = checkStr(partes[i]);
+                if (isNumeric) {
+                    double d = Double.parseDouble(partes[i]);
+                    numeros.add(d);
+                    System.out.println(numeros);
+                } else {
+                    byte[] resposta = "URL inválido".getBytes();
+                    exchange.sendResponseHeaders(200, resposta.length);
+                    exchange.getResponseBody().write(resposta);
+                    return;
+                }
+            }
+            double soma = numeros.stream().mapToDouble(Double::doubleValue).sum();
+            byte[] resposta = Double.toString(soma).getBytes();
             exchange.sendResponseHeaders(200, resposta.length);
             exchange.getResponseBody().write(resposta);
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             exchange.close();
         }
     }
-    
+
+    private boolean checkStr(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
 }
