a2b28e8d6b02f9c2e5b8156bba52eab1609eaf72
giovanaccorreia <glpc1@aluno.ifal.edu.br>
2022-07-26 23:13:37 +0000

    Testes para realizar multiplicação

--- api/src/main/java/app/Multiplicacao.java
+++ api/src/main/java/app/Multiplicacao.java
@@ -3,13 +3,13 @@ package app;
 import io.jooby.annotations.*;
 import io.jooby.exception.BadRequestException;
 
-@Path("/soma/{num1}/{num2}")
-public class Soma {
+@Path("/multiplicacao/{num1}/{num2}")
+public class Multiplicacao {
 
     @GET
-    public double calculaSoma(@PathParam String num1, @PathParam String num2) {
+    public double calculaMultiplicacao(@PathParam String num1, @PathParam String num2) {
         try {
-          return Float.parseFloat(num1)+Float.parseFloat(num2);
+          return Float.parseFloat(num1)*Float.parseFloat(num2);
         } catch (NumberFormatException nfe) {
           throw new BadRequestException("Parâmetros inválidos: " + num1 + "" + num2);
         }
